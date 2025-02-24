19f993acd7d15c977931b6d247a6c6c838dedff8
LWT0911 <lauwingtung11@yahoo.com.hk>
2025-02-25 01:02:20 +0800

    new

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ function openModal(imageSrc) {
 function closeModal() {
     document.getElementById('myModal').style.display = "none";
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
